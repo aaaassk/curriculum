48ff2ced4e811398156c22d1ee542d6632a69f26
aaaassk <77767109+aaaassk@users.noreply.github.com>
2021-09-22 14:56:06 +0900

    課題提出テスト
    Calculator.java,Main.java,Task.javaの作成

---
+++ JavaStudy/2-3/src/study/Calculator.java
@@ -0,0 +1,18 @@
+package study;
+
+public class Calculator {
+
+    protected int plus(int a) {
+        return a + 1;
+    }
+
+    protected int plus1(int a, int b) {
+        return a + b;
+    }
+
+    protected int plus2(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
